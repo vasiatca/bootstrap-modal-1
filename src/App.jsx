af3e1041bccfe01f541adee3d5f5deb088559987
vasiatca <vasiatca@yandex.ru>
2023-01-17 21:55:22 +0300

    feat(react): Button, props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Container from './components/Container'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Button from './components/Button'
 
 export function App() {
   return (
     <Container>
       <Header />
+      <Button data-bs-toggle="modal" data-bs-target="#exampleModal">Launch demo modal</Button>
       <Footer />
     </Container>
   )

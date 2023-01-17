d97f692e9f0f78f78d1bbbbc648ef4f115d57472
vasiatca <vasiatca@yandex.ru>
2023-01-17 21:58:36 +0300

    feat(react): advanced button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,11 @@
-const Button = (props) => {
+const Button = ({ size = '', ...props}) => {
+  const classSize = size && `btn-${size}`
+  // если true -> возвращается последнее значение
+
   return (
-    <button type="button" className="btn btn-primary" {...props}>
+    <button type="button" className={`btn btn-primary ${classSize}`.trim()} {...props}>
       {props.children}
     </button>
   )
 }
-
 export default Button
